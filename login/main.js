0b7a70b2f88bfa8296d5fdcdf490afab8c7e8506
Morgan Roberts <morgrob@email.unc.edu>
2021-04-07 12:50:09 -0400

    Improved visibility toggle on login page, edits on all other pages

--- login/main.js
+++ login/main.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    var x = document.getElementById("myInput");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
   }
+}
